5ab94d3d8bcec7f0ceabc9856d25b1c971cf9cd3
GErickOBTS <gerboxd15.math@gmail.com>
2026-05-12 12:30:19 -0500

    cambios en main.js para importacion de storage.js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,17 @@
 
 import { dibujarRuleta } from './ruleta.js';
 import { inicializarInterfazSorteo } from './interface.js';
+import { inicializarTextAreaRuleta } from './storage.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Aplicación Práctica 2 inicializada");
 
+    // Callback para actualizar la ruleta cuando cambia el textarea
+    const actualizarRuleta = (sectores) => {
+        dibujarRuleta('ruleta-container', sectores);
+    };
+
     // Inicializar componentes base
-    dibujarRuleta('ruleta-container', []);
+    inicializarTextAreaRuleta('ruleta-container', actualizarRuleta);
     inicializarInterfazSorteo('sorteo-container');
 });
